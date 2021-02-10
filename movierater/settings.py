f89fc20b499aa504f8b23bb5b7f10ded41029358
unknown <islandhuynh@gmail.com>
2021-02-09 23:12:48 -0500

    Added heroku as an allowed host

--- movierater/settings.py
+++ movierater/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['192.168.0.18']
+ALLOWED_HOSTS = ['192.168.0.18', 'island-movie-rater.herokuapp.com']
 
 
 # Application definition
